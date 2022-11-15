94bbb9a44b56d0d373727d9c877b07d45187c464
Isaiah <isaiah@swoopnow.com>
2022-11-15 14:15:55 -0700

    hosts

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module SwoopWebhooks
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.hosts << "isaiahbaca.ngrok.io"
+    config.hosts << "swoop-webook-demo.herokuapp.com"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
