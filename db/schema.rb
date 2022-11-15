ed12864019bd658e91d5ff64f6a97d3a0c3bc466
Isaiah <isaiah@swoopnow.com>
2022-11-15 14:27:44 -0700

    fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2022_11_15_211841) do
 
-  create_table "inbound_hooks", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.text "payload"
-  end
-
   create_table "incoming_hooks", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
